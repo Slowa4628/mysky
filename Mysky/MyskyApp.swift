3f1e545cde7d59ff2ffe9f4c022f4151e860698d
Stanislav Konchakovsky <boks3535@gmail.com>
2025-10-16 22:05:41 +0300

    Initial Commit

---
+++ Mysky/MyskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyskyApp.swift
+//  Mysky
+//
+//  Created by Стас on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
